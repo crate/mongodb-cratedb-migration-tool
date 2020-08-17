3e340e97ee7b6a4e01e0b38772c926d2eacc8a69
Mika Naylor <mail@autophagy.io>
2020-08-17 10:11:20 +0200

    Add changelog, better structure, flesh out setup.y

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
+import io
+import os
+import re
+
 from setuptools import find_packages, setup
 
+
+def read(path):
+    path = os.path.join(os.path.dirname(__file__), path)
+    with io.open(path, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+versionf_content = read(os.path.join("crate", "migr8", "__init__.py"))
+version_rex = r'^__version__ = [\'"]([^\'"]*)[\'"]$'
+m = re.search(version_rex, versionf_content, re.M)
+if m:
+    version = m.group(1)
+else:
+    raise RuntimeError("Unable to find version string")
+
+
 setup(
-    name="rip",
-    entry_points={"console_scripts": ["rip = rip.__main__:main"]},
-    packages=find_packages(),
-    install_requires=[
-        "pymongo==3.10.1",
-        "rich==3.3.2",
-    ],
+    name="migr8",
+    version=version,
+    url="https://github.com/crate/mongodb-cratedb-migration-tool",
+    author="Crate.io",
+    author_email="office@crate.io",
+    description="MongoDB -> CrateDB Migration Tool",
+    long_description=read("README.rst"),
+    platforms=["any"],
+    license="Apache License 2.0",
+    packages=["crate.migr8"],
+    namespace_packages=["crate"],
+    entry_points={"console_scripts": ["migr8 = crate.migr8.__main__:main"]},
+    install_requires=["pymongo==3.10.1", "rich==3.3.2",],
     extras_require={
         "testing": ["black==18.9b0", "flake8==3.7.7", "mypy==0.670", "isort==4.3.15"]
     },
